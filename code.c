77551f9f2bee04835251e63da20591525961c371
rajiitb34 <rajiitb34@gmail.com>
2017-06-09 20:51:14 +0530

    Create code.c

--- code.c
+++ code.c
@@ -1,1 +1,12 @@
-printf("hi");
+#include<stdio.h>
+int main(){
+  int number;
+  scanf("%d",&number);
+  if(number>0)
+    printf("positive number");
+  else if(number<0)
+    printf("negative number");
+  else
+    printf("0");
+  return 0;
+}
